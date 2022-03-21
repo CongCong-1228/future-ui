01e7c356f55ccbaebcbb3c7f98a06442b3903628
congcong1228 <fivesun1228@163.com>
2022-03-21 18:50:38 +0800

    Dialog组件基本完成

--- src/components/DiglogDemo.vue
+++ src/components/DiglogDemo.vue
@@ -1,12 +1,312 @@
 <template>
-  <Dialog></Dialog>
+  <h1 class="title">Dialog对话框</h1>
+  <div class="tips"><p>在保留当前页面状态的情况下，告知用户并承载相关操作。</p></div>
+  <div class="top"><h2 style="font-weight: 600;">基本用法</h2></div>
+  <div class="tips"><p>Dialog 弹出一个对话框，适合需要定制性更大的场景。</p></div>
+  <section>
+    <FutureButton class="future-button" @click="toggle">点击打开dialog</FutureButton>
+    <FutureDialog v-model:visible="visible"></FutureDialog>
+    <div class="bottom">
+      <div class="button" @click="showCode">查看代码</div>
+    </div>
+    <div class="code" :class="{show}">
+      <div class="description">
+        <p>需要设置<span style="color:#31a354;">visible</span>属性，他接收<span style="color:#31a354;">Boolean</span>，当为<span
+            style="color:#31a354;">true</span>时显示Dialog。</p>
+      </div>
+      <pre> <code class="hljs">
+<span class="hljs-tag"><span class="hljs-name">&lt;futureButton</span> <span class="hljs-name">@click</span>=<span
+    class="hljs-attr">toggle</span>&gt;点击打开diolog&lt;<span class="hljs-name">/futureButton&gt;</span></span>
+<span class="hljs-tag"><span class="hljs-name">&lt;futureDialog</span> <span
+          class="hljs-name">v-model:visible</span>=<span
+          class="hljs-attr">visible&gt;</span></span><span class="hljs-tag"><span class="hljs-name">&lt;/futureDialog&gt;</span></span>
+
+<span class="hljs-tag"><span class="hljs-name">&lt;script&gt;</span></span>
+<span class="hljs-tag"><span class="hljs-name">import FutureButton from fu-button</span>
+<span class="hljs-name">import FutureDialog from fu-dialog</span>
+<span class="hljs-name">import {ref} from 'vue'</span>
+ <span class="hljs-name">export default {</span>
+    <span class="hljs-name">components: {FutureDialog, FutureButton}</span>
+    <span class="hljs-name">setup() {</span>
+    <span class="hljs-name">const visible = ref(false)</span>
+    <span class="hljs-name">const toggle = () => {</span>
+    <span class="hljs-name">visible.value = !visible.value</span>
+    <span class="hljs-name">}</span>
+      <span class="hljs-name">return {visible, toggle}</span>
+  <span class="hljs-name">}</span>
+<span class="hljs-name">&lt;/script&gt;</span>
+
+</span>
+      </code>
+      </pre>
+    </div>
+  </section>
+
+
+  <div class="top"><h2 style="font-weight: 600;">自定义标题或内容</h2></div>
+  <div class="tips"><p>你可以自定义标题和内容</p></div>
+  <section>
+    <FutureButton class="future-button" @click="toggle1">点击打开dialog</FutureButton>
+    <FutureDialog v-model:visible1="visible1">
+      <template v-slot:title>
+        <strong>你自定义的标题</strong>
+      </template>
+      <template v-slot:content>
+        <p>这是你可以自定义的内容</p>
+      </template>
+    </FutureDialog>
+    <div class="bottom">
+      <div class="button" @click="showCode1">查看代码</div>
+    </div>
+    <div class="code" :class="{show1}">
+      <div class="description">
+        <p>需要设置<span style="color:#31a354;">visible</span>属性，他接收<span style="color:#31a354;">Boolean</span>，当为<span
+            style="color:#31a354;">true</span>时显示Dialog。
+          支持自定义 <span style="color:#31a354;">title</span>，<span style="color:#31a354;">content</span>的样式。</p>
+      </div>
+      <pre> <code class="hljs">
+<span class="hljs-tag"><span class="hljs-name">&lt;futureButton</span> <span class="hljs-name">@click</span>=<span
+    class="hljs-attr">toggle</span>&gt;点击打开diolog&lt;<span
+    class="hljs-name">/futureButton&gt;</span></span>
+<span class="hljs-tag"><span class="hljs-name">&lt;futureDialog</span> <span
+    class="hljs-name">v-model:visible</span>=<span
+    class="hljs-attr">visible&gt;</span></span>
+    <span class="hljs-tag"><span class="hljs-name">&lt;template</span> <span class="hljs-name">v-slot:title&gt;</span>
+    <span class="hljs-name">&lt;strong&gt;</span>你自定义的标题<span class="hljs-name">&lt;/strong&gt;</span>
+    <span class="hljs-attr">&lt;/template&gt;</span></span>
+    <span class="hljs-tag"><span class="hljs-name">&lt;template</span> <span class="hljs-name">v-slot:content&gt;</span>
+    <span class="hljs-name">&lt;strong&gt;</span>这是你可以自定义的内容<span class="hljs-name">&lt;/strong&gt;</span>
+    <span class="hljs-attr">&lt;/template&gt;</span></span>
+<span class="hljs-tag"><span class="hljs-name">&lt;/futureDialog&gt;</span></span>
+
+<span class="hljs-tag"><span class="hljs-name">&lt;script&gt;</span></span>
+<span class="hljs-tag"><span class="hljs-name">import FutureButton from fu-button</span>
+<span class="hljs-name">import FutureDialog from fu-dialog</span>
+<span class="hljs-name">import {ref} from 'vue'</span>
+ <span class="hljs-name">export default {</span>
+    <span class="hljs-name">components: {FutureDialog, FutureButton}</span>
+    <span class="hljs-name">setup() {</span>
+    <span class="hljs-name">const visible = ref(false)</span>
+    <span class="hljs-name">const toggle = () => {</span>
+    <span class="hljs-name">visible.value = !visible.value</span>
+    <span class="hljs-name">}</span>
+      <span class="hljs-name">return {visible, toggle}</span>
+  <span class="hljs-name">}</span>
+<span class="hljs-name">&lt;/script&gt;</span>
+
+</span>
+      </code>
+      </pre>
+    </div>
+  </section>
+
 </template>
 
 
 <script lang="ts">
-import Dialog from '../libs/Dialog.vue'
+import FutureDialog from '../libs/Dialog.vue'
+import FutureButton from '../libs/Button.vue'
+import {ref} from "vue";
 
 export default {
-  components: {Dialog}
+  components: {FutureDialog, FutureButton},
+  setup() {
+    const visible = ref(false)
+    const visible1 = ref(false)
+    const toggle = () => {
+      visible.value = !visible.value
+    }
+    const toggle1 = () => {
+      visible1.value = !visible1.value
+    }
+    const show = ref(false)
+    const showCode = () => {
+      show.value = !show.value
+    }
+    const show1 = ref(false)
+    const showCode1 = () => {
+      show1.value = !show1.value
+    }
+    return {visible, visible1, toggle, toggle1, show, showCode, show1, showCode1}
+  }
 }
 </script>
+
+<style lang="scss">
+.title {
+  margin-bottom: 20px;
+  font-weight: 600;
+  font-size: 36px;
+}
+
+.tips {
+
+  > p {
+    font-size: 18px;
+    color: #5e6d82;
+    line-height: 1.5em;
+  }
+}
+
+.top {
+  display: flex;
+  align-items: center;
+  padding: 20px 20px 10px 0;
+  margin-top: 20px;
+}
+
+section {
+  .future-button {
+    margin: 20px;
+  }
+
+  border: 1px solid #ebebeb;
+  margin-bottom: 40px;
+  margin-top: 20px;
+
+  > .bottom {
+    font-size: 24px;
+    width: 100%;
+    padding: 10px 24px;
+    display: flex;
+    align-items: center;
+    border-top: 1px solid #ebebeb;
+
+    > .button {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 6px 8px;
+      border-radius: 12px;
+      border: 1px solid #ebebeb;
+      font-size: 14px;
+
+      &:hover {
+        cursor: pointer;
+        color: #409eff;
+
+      }
+    }
+  }
+
+  > .code {
+    height: 0;
+    transition: all .3s;
+
+    > .description {
+      display: none;
+      padding: 20px;
+      box-sizing: border-box;
+      border: 1px solid #ebebeb;
+      border-radius: 3px;
+      font-size: 14px;
+      line-height: 22px;
+      color: #666;
+      word-break: break-word;
+      margin: 10px;
+      background-color: #fff;
+
+      > p {
+        color: #5e6d82;
+
+      }
+    }
+
+    > pre {
+      > .hljs {
+        display: none;
+      }
+    }
+
+    &.show {
+      height: 550px;
+      //border-top: 1px solid #ebebeb;
+      > .description {
+        display: block;
+      }
+
+      > pre {
+        //line-height: 1.1;
+        //margin: 0;
+
+        > .hljs {
+          display: block;
+          line-height: 1.8;
+          font-family: Menlo, Monaco, Consolas, Courier, monospace;
+          font-size: 12px;
+          padding: 18px 24px;
+          background-color: #fafafa;
+          border: 1px solid #eaeefb;
+          margin-bottom: 25px;
+          border-radius: 4px;
+          -webkit-font-smoothing: auto;
+          @media (max-width: 500px) {
+            overflow-x: auto;
+          }
+
+          > .hljs-tag {
+
+            > .hljs-name {
+              color: #3182bd;
+            }
+
+            > .hljs-attr {
+              color: #3182bd;
+            }
+
+            > .hljs-string {
+              color: #756bb1
+            }
+          }
+        }
+      }
+    }
+
+    &.show1 {
+      height: 400px;
+      //border-top: 1px solid #ebebeb;
+      > .description {
+        display: block;
+      }
+
+      > pre {
+        //line-height: 1.1;
+        //margin: 0;
+
+        > .hljs {
+          display: block;
+          line-height: 1.8;
+          font-family: Menlo, Monaco, Consolas, Courier, monospace;
+          font-size: 12px;
+          padding: 18px 24px;
+          background-color: #fafafa;
+          border: 1px solid #eaeefb;
+          margin-bottom: 25px;
+          border-radius: 4px;
+          -webkit-font-smoothing: auto;
+          @media (max-width: 500px) {
+            overflow-x: auto;
+          }
+
+          > .hljs-tag {
+
+            > .hljs-name {
+              color: #3182bd;
+            }
+
+            > .hljs-attr {
+              color: #3182bd;
+            }
+
+            > .hljs-string {
+              color: #756bb1
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+}
+</style>
--- src/libs/Dialog.vue
+++ src/libs/Dialog.vue
@@ -1,22 +1,141 @@
 <template>
-  <div class="dialog-overlay"></div>
-  <div class="dialog-wrapper">
-    <header>标题</header>
-    <main>
-      <p>第一行字</p>
-      <p>第二行字</p>
-    </main>
-    <footer>
-      <Button>OK</Button>
-      <Button>Cancel</Button>
-    </footer>
-  </div>
-</template>
+  <template v-if="visible || visible1">
+    <Teleport to="body">
+      <div class="fu-dialog-overlay"></div>
+      <div class="fu-dialog-wrapper">
+        <div class="fu-dialog">
+          <header>
+            <span v-if="visible">提示</span>
+            <slot v-if="visible1" name="title"/>
+            <span class="fu-dialog-close" @click="close"></span></header>
+          <main>
+            <p v-if="visible">这是默认的内容</p>
+            <slot v-if="visible1" name="content"/>
+          </main>
+          <footer>
+            <Button theme="button" plain @click="close">取消</Button>
+            <Button theme="primary" @click="close">确认</Button>
+          </footer>
+        </div>
+      </div>
+    </Teleport>
+  </template>
 
+</template>
 
 <script lang="ts">
 import Button from './Button.vue'
+
 export default {
-  components: {Button}
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    visible1: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  components: {Button},
+  setup(props, context) {
+    const close = () => {
+      if (props.visible) {
+        context.emit('update:visible', false)
+      }
+      if (props.visible1) {
+        context.emit('update:visible1', false)
+      }
+    }
+
+    return {close}
+  }
 }
 </script>
+
+
+<style lang="scss">
+$radius: 4px;
+$border-color: #d9d9d9;
+.fu-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+
+  &-wrapper {
+    width: 30%;
+    position: fixed;
+    left: 50%;
+    top: 40%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+    height: 30%;
+  }
+
+  > header {
+    padding: 12px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+
+  > main {
+    padding: 30px 20px;
+
+    > p {
+      color: #606266;
+      font-size: 14px;
+      word-break: break-all;
+    }
+  }
+
+  > footer {
+    padding: 12px 20px;
+    text-align: right;
+
+    > Button {
+      margin-right: 10px;
+    }
+  }
+
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+}
+</style>
